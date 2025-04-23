f2bc25c61e87f968a423da96989eddc83c4bf6f6
Clayon <admin@github.xyz>
2025-04-23 21:16:55 +0800

    feat: update memory

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -18,12 +18,12 @@ export class HealthController {
   @HealthCheck()
   check() {
     return this.healthCheckService.check([
-      // the process should not use more than 300M memory
+      // the process should not use more than 600M memory
       () =>
-        this.memoryHealthIndicator.checkHeap('memory heap', 300 * 1024 * 1024),
-      // The process should not have more than 300M RSS memory allocated
+        this.memoryHealthIndicator.checkHeap('memory heap', 600 * 1024 * 1024),
+      // The process should not have more than 600M RSS memory allocated
       () =>
-        this.memoryHealthIndicator.checkRSS('memory RSS', 300 * 1024 * 1024),
+        this.memoryHealthIndicator.checkRSS('memory RSS', 600 * 1024 * 1024),
     ]);
   }
 }
